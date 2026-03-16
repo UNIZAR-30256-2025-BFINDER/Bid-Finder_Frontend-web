da74da0615b8779051f3167e18488cebf06e7c3e
PabloVilla199 <pablovillacamanes@gmail.com>
2026-03-16 22:38:22 +0100

    feat(PB5): implementar clustering, dark mode y popup en el mapa (#10, #11, #12, #13)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { MainLayout } from './components/MainLayout';
-import { LandingPage } from './pages/LandingPage';
+import { MainLayout } from './components/layout/MainLayout';
+
+import { LandingPage } from './features/landing/pages/LandingPage';
+import Admin from './features/dashboard/pages/Admin';
+import DashBoard from './features/map/pages/DashBoard';
+import Favorites from './features/dashboard/pages/Favorites';
+import SubastaDetail from './features/subastas/pages/SubastaDetail';
 
 const router = createBrowserRouter(
-    [
-        {
-            path: '/',
-            element: <MainLayout />,
-            children: [
-                { index: true, element: <LandingPage /> },
-            ],
-        },
-    ],
+  [
     {
-        basename: import.meta.env.BASE_URL,
-    }
+      path: '/',
+      element: <MainLayout />,
+      children: [
+        { index: true, element: <LandingPage /> },
+        { path: 'admin', element: <Admin /> },
+        { path: 'dashboard', element: <DashBoard /> },
+        { path: 'favorites', element: <Favorites /> },
+        { path: 'subasta/:id', element: <SubastaDetail /> },
+      ],
+    },
+  ],
+  {
+    basename: import.meta.env.BASE_URL,
+  },
 );
 
 export const App: React.FC = () => {
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './App'
-import './styles.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+import './styles.css';
+import 'leaflet/dist/leaflet.css';
+import './leaflet-popup-overrides.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
